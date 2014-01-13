92ea51d729ceb72eba8c1d8448ba997e134ec1a4
Nate <namoricoo@hotmail.com>
2014-01-12 18:37:36 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuakismartBootstrap::Application.config.secret_key_base = '7207e2533249186dcf67a08cd7d169b9960af289692b9e8711410637f9428032114f57190e146fe819a5eaad35f014b56ef56a41f4dd80088d2defdd0789f412'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuakismartBootstrap::Application.config.session_store :cookie_store, key: '_quakismart_bootstrap_session'
