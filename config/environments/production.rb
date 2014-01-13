47d7a731a0d02936c49378d893cfd76cbc1f7629
Nate <namoricoo@hotmail.com>
2014-01-13 14:24:18 -0800

    config.serve_static_assets = true to stop heroku from compressing images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,9 +18,7 @@ QuakismartBootstrap::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+ 
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -78,7 +76,8 @@ QuakismartBootstrap::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  
+  # for Heroku
+  config.serve_static_assets = true
   # How to load css.erb files through asset pipeline
   config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 end
