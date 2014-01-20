0a98b3d1e30058f01d74de1d0c98d618d1a58532
Nate <namoricoo@hotmail.com>
2014-01-19 19:10:21 -0800

    add custom 404 error page and custom 500 error page

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,7 +1,15 @@
 class ErrorsController < ApplicationController
   def not_found
-  end
+    respond_to do |format|
+      format.html { render template: 'errors/not_found', layout: 'layouts/application', status: 404 }
+      format.all  { render nothing: true, status: 404 }
+    end
+  end  
 
   def server_error
+    respond_to do |format|
+      format.html { render template: 'errors/server_error', layout: 'layouts/application', status: 500 }
+      format.all  { render nothing: true, status: 500}
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ QuakismartBootstrap::Application.routes.draw do
   get 'usgs/jquery'
   get 'usgs/jqueryui'
   post 'usgs/search'
+  if Rails.env.production?
+    get "/404", :to => "errors#not_found"
+    get "/500", :to => "errors#server_error" 
+  end  
 end
