fc30057c549390294d8cdcbf9e27ab7e7daa6708
Nate <namoricoo@hotmail.com>
2014-02-18 23:26:20 -0800

    update usgs search spec

--- spec/features/search_usgs_spec.rb
+++ spec/features/search_usgs_spec.rb
@@ -4,23 +4,23 @@ describe "is able to search quakismart" do
   before :each do
      visit '/usgs/index'
   end
-    
+
   it 'should fill in magnitude value' do     
      fill_in 'magnitude_range',with: '0;0'
   end
-  
+
   it 'should fill in felt_range value' do     
      fill_in 'felt_range',with: '0;0'
   end
-  
+
   it 'should fill in dimension_range value' do     
      fill_in 'dimension_range',with: '0;0'
   end
-  
+
   it 'should fill in dimension_range value' do     
      fill_in 'cdi_range',with: '0;0'
   end
-  
+
   it 'should click reset Link' do     
      click_link 'Reset'
   end  
